54179780abee6c78e20ba653ab3f1228aafcf9f3
Gonzalo Raymond <gonchi_msn@hotmail.com>
2022-08-29 10:22:57 -0300

    HTML y CSS completos

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/* ============================ Typing Animation ============================ */
+
+var typed = new Typed(".typing", {
+    strings:["", "Diseñador web", "Desarrollador Front-End", "Desarrollador web"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+});
